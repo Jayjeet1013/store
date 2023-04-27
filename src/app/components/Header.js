1c631a54da141be6531485036ef1088065c7af74
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-27 16:31:19 +0530

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='bg-black text-white '>
-        <h1 className=' font-bold text-center text-white' ></h1>
-        Header
+    <div className ='bg-green-500 text-white h-16  p-4  ' >
+       <Image src='/Amazon-logo.png' width={500} height={500} className='w-24 h-30 ' alt='logo'/>
+        <h1 className=' font-bold text-left  text-black' > Hello select your address</h1>
+        
+        
         </div>
   )
 }
