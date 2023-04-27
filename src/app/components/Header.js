d37369d5a152fbb15f2ebc10a4fa678f36a1704a
Jayjeet1013 <jayjeet1013@gmail.com>
2023-04-27 17:00:54 +0530

    update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className ='bg-green-500 text-white h-16  p-4  ' >
-       <Image src='/Amazon-logo.png' width={500} height={500} className='w-24 h-30 ' alt='logo'/>
-        <h1 className=' font-bold text-left  text-black' > Hello select your address</h1>
+    <div className ='bg-green-400 text-white h-16  p-4 flex  ' >
+       <Image src='/Amazon-logo.png' width={500} height={500} className='w-24 h-30 cursor-pointer ' alt='logo'/>
+        <h1 className=' font-bold ml-9 cursor-pointer text-xs text-black'  > Hello 
+   
+        <span className='flex cursor-pointer text-lg'>      <Image src='/location-logo.png' width={500} height={500}  className='w-6 h-7 flex' alt='logo'/>select your address</span></h1>
         
         
         </div>
