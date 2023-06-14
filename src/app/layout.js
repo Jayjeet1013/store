bd236069b682203fe978ee241baf9638f4b71816
Jayjeet1013 <jayjeet1013@gmail.com>
2023-06-14 23:05:46 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,8 @@
+import Miniheader from './components/Miniheader'
 import './globals.css'
+import Header from './components/Header'
+import Body from './components/Body'
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <Header/>
+      <Miniheader/>
+        {children}
+        <Body/>
+        </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,7 @@ const page = () => {
   return (
     <div>
 
-      <Header/>
-      <Miniheader/>
-      <Body/>
+    
     </div>
 
 
